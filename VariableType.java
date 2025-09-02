071051d280420af8eaed63ffecb9ff04a2a4b75e
AayuAmor <aayuamor@gmail.com>
2025-09-02 15:17:46 +0545

    Add VariableType class with display method and main function

---
+++ VariableType.java
@@ -0,0 +1,30 @@
+public class VariableType {
+    int age;
+    
+    static int num1 = 98;
+    void display(){ //Local variable
+        String name = "hel";
+        System.out.println("name = " + name + "Age = "+age + "Num = " +num1);
+    }
+    /*
+    void display2(){
+        System.out.println("Name" + name);
+    }
+    */
+    public static void main(String[] args) {
+        VariableType variableType = new VariableType();
+        
+        variableType.display();
+        variableType.age = 67;
+        VariableType.num1 = 2;
+
+        variableType.display();
+        VariableType variableType2 = new VariableType();
+        variableType2.display();
+        variableType2.age=7;
+        VariableType.num1 =8;
+        variableType2.display();
+        
+        System.out.println(VariableType.num1);
+    }
+}
