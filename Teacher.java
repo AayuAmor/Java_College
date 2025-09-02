0dfb5890fd7db86979a148ea4fcdb8bcbf60fc9e
AayuAmor <aayuamor@gmail.com>
2025-09-02 15:16:11 +0545

    Add Teacher class with display method and main function

---
+++ Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    String name;
+    int age;
+    
+    void display(){
+        System.out.println("Name = " + name + "\nAge = " + age);
+    }
+
+    public static void main(String[] args) {
+        Teacher info = new Teacher();
+        info.name="Aayush";
+        info.age=22;
+        info.display();
+    }
+}
